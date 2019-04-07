4e28139c77f4180dd425e7edfe4ea385e339d786
Daniel S. Blanco <deesebc@gmail.com>
2019-04-07 11:31:16 +0200

    update method

--- HttpClientFluentExample/src/main/java/es/home/example/api/BookClient.java
+++ HttpClientFluentExample/src/main/java/es/home/example/api/BookClient.java
@@ -8,6 +8,7 @@ import org.apache.http.HttpVersion;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 
 import com.google.gson.Gson;
 
@@ -18,8 +19,8 @@ public class BookClient {
 	private final static String URL = "http://localhost:8080/JerseyExample/rest/book";
 
 	public List<Book> getAll() throws ClientProtocolException, IOException {
-		String jsonResponse = Request.Get(URL).connectTimeout(1000).socketTimeout(1000).execute()
-				.returnContent().asString();
+		String jsonResponse = Request.Get(URL).connectTimeout(1000).socketTimeout(1000).execute().returnContent()
+				.asString();
 		java.lang.reflect.Type listType = new com.google.gson.reflect.TypeToken<ArrayList<Book>>() {
 		}.getType();
 		List<Book> yourClassList = new Gson().fromJson(jsonResponse, listType);
@@ -28,39 +29,34 @@ public class BookClient {
 	// http://localhost:8080/JerseyExample/rest/book
 
 	public Book getOne(final Integer id) throws ClientProtocolException, IOException {
-		String jsonResponse = Request.Get(URL+"/"+id).connectTimeout(1000).socketTimeout(1000).execute()
+		String jsonResponse = Request.Get(URL + "/" + id).connectTimeout(1000).socketTimeout(1000).execute()
 				.returnContent().asString();
-//		jsonResponse = jsonResponse.substring(1, jsonResponse.length()-1);
+		// jsonResponse = jsonResponse.substring(1, jsonResponse.length()-1);
 		return new Gson().fromJson(jsonResponse, Book.class);
 	}
 	// // http://localhost:8080/JerseyExample/rest/book/2
-	
-	 public int deleteOne( final Integer id) throws ClientProtocolException, IOException {
-		 return Request.Delete(URL+"/"+id)
-	        .useExpectContinue()
-	        .version(HttpVersion.HTTP_1_1)
-	        .bodyString("Important stuff", ContentType.DEFAULT_TEXT)
-	        .execute().returnResponse().getStatusLine().getStatusCode();
-	 }
-	 // http://localhost:8080/JerseyExample/rest/book/2
-	
-	 public int updateOne(final Integer id, final Book book) throws ClientProtocolException, IOException {
-		 return Request.Put(URL+"/"+id)
-			        .useExpectContinue()
-			        .version(HttpVersion.HTTP_1_1)
-			        .bodyString("Important stuff", ContentType.DEFAULT_TEXT)
-			        .execute().returnResponse().getStatusLine().getStatusCode();
-	 }
-	 // http://localhost:8080/JerseyExample/rest/book/2 . Body:
-	 // {"id":4,"author":"Frederik Pohl","name":"Gateway"}
-	
-	// @POST
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public Response saveOne(final Book book) {
-	// bookMap.put(bookMap.size() + 1, book);
-	// return Response.status(200).build();
-	// }
-	// // http://localhost:8080/JerseyExample/rest/book . Body:
-	// // {"id":4,"author":"Frederik Pohl","name":"Gateway"}
+
+	public int deleteOne(final Integer id) throws ClientProtocolException, IOException {
+		return Request.Delete(URL + "/" + id).useExpectContinue().execute().returnResponse().getStatusLine()
+				.getStatusCode();
+	}
+	// http://localhost:8080/JerseyExample/rest/book/2
+
+	public int updateOne(final Integer id, final Book book) throws ClientProtocolException, IOException {
+		String bookJson = new Gson().toJson(book);
+		StringEntity requestEntity = new StringEntity(bookJson, ContentType.APPLICATION_JSON);
+		return Request.Put(URL + "/" + id).body(requestEntity).useExpectContinue().version(HttpVersion.HTTP_1_1)
+				.execute().returnResponse().getStatusLine().getStatusCode();
+	}
+	// http://localhost:8080/JerseyExample/rest/book/2 . Body:
+	// {"id":4,"author":"Frederik Pohl","name":"Gateway"}
+
+	public int saveOne(final Book book) throws ClientProtocolException, IOException {
+		String bookJson = new Gson().toJson(book);
+		return Request.Post(URL).bodyString(bookJson, ContentType.APPLICATION_JSON).execute().returnResponse()
+				.getStatusLine().getStatusCode();
+	}
+	// http://localhost:8080/JerseyExample/rest/book . Body:
+	// {"id":4,"author":"Frederik Pohl","name":"Gateway"}
 
 }
--- HttpClientFluentExample/src/test/resources/test/es/home/example/api/TestBookClient.java
+++ HttpClientFluentExample/src/test/resources/test/es/home/example/api/TestBookClient.java
@@ -6,8 +6,14 @@ import static org.hamcrest.Matchers.equalTo;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.http.HttpVersion;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.junit.Test;
+
+import com.google.gson.Gson;
 
 import es.home.example.api.BookClient;
 import es.home.example.pojo.Book;
@@ -15,19 +21,43 @@ import es.home.example.pojo.Book;
 public class TestBookClient {
 
 	BookClient client = new BookClient();
-	
+
 	@Test
-	public void getAll() throws ClientProtocolException, IOException{
+	public void getAll() throws ClientProtocolException, IOException {
 		List<Book> list = client.getAll();
 		assertThat(list.size(), equalTo(3));
 	}
-	
+
 	@Test
-	public void getOne() throws ClientProtocolException, IOException{
+	public void getOne() throws ClientProtocolException, IOException {
 		Book two = client.getOne(2);
 		assertThat(two.getId(), equalTo(2));
+		assertThat(two.getName(), equalTo("The stars my destination"));
 	}
-	
 
+	@Test
+	public void saveOne() throws ClientProtocolException, IOException {
+		Book book = new Book(4, "F. Herbert", "Dune");
+		int returnCode = client.saveOne(book);
+		assertThat(returnCode, equalTo(200));
+	}
+
+	@Test
+	public void updateOne() throws ClientProtocolException, IOException {
+		Book book = new Book(4, "Frank Herbert", "Dune");
+		int returnCode = client.updateOne(4, book);
+		assertThat(returnCode, equalTo(200));
+		Book two = client.getOne(4);
+		assertThat(two.getId(), equalTo(4));
+		assertThat(two.getAuthor(), equalTo("Frank Herbert"));
+	}
+
+	@Test
+	public void deleteOne() throws ClientProtocolException, IOException {
+		int returnCode = client.deleteOne(4);
+		assertThat(returnCode, equalTo(200));
+		List<Book> list = client.getAll();
+		assertThat(list.size(), equalTo(3));
+	}
 
 }

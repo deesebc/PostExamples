bfa5be6927f904fdd9473f619f8b59f784954849
Daniel S. Blanco <deesebc@gmail.com>
2019-04-18 22:11:42 +0200

    update

--- HttpClientFluentExample/src/main/java/es/home/example/api/BookClient.java
+++ HttpClientFluentExample/src/main/java/es/home/example/api/BookClient.java
@@ -19,10 +19,8 @@ public class BookClient {
 	private final static String URL = "http://localhost:8080/JerseyExample/rest/book";
 
 	public List<Book> getAll() throws ClientProtocolException, IOException {
-		String jsonResponse = Request.Get(URL).connectTimeout(1000).socketTimeout(1000).execute().returnContent()
-				.asString();
-		java.lang.reflect.Type listType = new com.google.gson.reflect.TypeToken<ArrayList<Book>>() {
-		}.getType();
+		String jsonResponse = Request.Get(URL).execute().returnContent().asString();
+		java.lang.reflect.Type listType = new com.google.gson.reflect.TypeToken<ArrayList<Book>>() {}.getType();
 		List<Book> yourClassList = new Gson().fromJson(jsonResponse, listType);
 		return yourClassList;
 	}
@@ -31,32 +29,32 @@ public class BookClient {
 	public Book getOne(final Integer id) throws ClientProtocolException, IOException {
 		String jsonResponse = Request.Get(URL + "/" + id).connectTimeout(1000).socketTimeout(1000).execute()
 				.returnContent().asString();
-		// jsonResponse = jsonResponse.substring(1, jsonResponse.length()-1);
 		return new Gson().fromJson(jsonResponse, Book.class);
 	}
 	// // http://localhost:8080/JerseyExample/rest/book/2
+
+	public int saveOne(final Book book) throws ClientProtocolException, IOException {
+		String bookJson = new Gson().toJson(book);
+		return Request.Post(URL).bodyString(bookJson, ContentType.APPLICATION_JSON).useExpectContinue()
+				.version(HttpVersion.HTTP_1_1).execute().returnResponse().getStatusLine().getStatusCode();
+	}
+	// http://localhost:8080/JerseyExample/rest/book . Body:
+	// {"id":4,"author":"Frederik Pohl","name":"Gateway"}
+
+	public int updateOne(final Integer id, final Book book) throws ClientProtocolException, IOException {
+		String bookJson = new Gson().toJson(book);
+		// StringEntity requestEntity = new StringEntity(bookJson, ContentType.APPLICATION_JSON);
+		StringEntity requestEntity = new StringEntity(bookJson);
+		return Request.Put(URL + "/" + id).body(requestEntity).addHeader("Conten-Type", "application/json").execute()
+				.returnResponse().getStatusLine().getStatusCode();
+	}
+	// http://localhost:8080/JerseyExample/rest/book/2 . Body:
+	// {"id":4,"author":"Frederik Pohl","name":"Gateway"}
 
 	public int deleteOne(final Integer id) throws ClientProtocolException, IOException {
 		return Request.Delete(URL + "/" + id).useExpectContinue().execute().returnResponse().getStatusLine()
 				.getStatusCode();
 	}
 	// http://localhost:8080/JerseyExample/rest/book/2
-
-	public int updateOne(final Integer id, final Book book) throws ClientProtocolException, IOException {
-		String bookJson = new Gson().toJson(book);
-		StringEntity requestEntity = new StringEntity(bookJson, ContentType.APPLICATION_JSON);
-		return Request.Put(URL + "/" + id).body(requestEntity).useExpectContinue().version(HttpVersion.HTTP_1_1)
-				.execute().returnResponse().getStatusLine().getStatusCode();
-	}
-	// http://localhost:8080/JerseyExample/rest/book/2 . Body:
-	// {"id":4,"author":"Frederik Pohl","name":"Gateway"}
-
-	public int saveOne(final Book book) throws ClientProtocolException, IOException {
-		String bookJson = new Gson().toJson(book);
-		return Request.Post(URL).bodyString(bookJson, ContentType.APPLICATION_JSON).execute().returnResponse()
-				.getStatusLine().getStatusCode();
-	}
-	// http://localhost:8080/JerseyExample/rest/book . Body:
-	// {"id":4,"author":"Frederik Pohl","name":"Gateway"}
 
 }

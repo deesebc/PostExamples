ef2a5d54985d005092686e710f8367b30adafaa9
deesebc <deesebc@gmail.com>
2020-09-05 19:35:08 +0200

    jpa

--- ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/pojo/Book.java
+++ ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/pojo/Book.java
@@ -1,5 +1,12 @@
 package com.example.home.ApacheCamelRestExample.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
@@ -11,9 +18,14 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity(name = "BOOK")
+@Table(name = "BOOK")
+@NamedQuery(name = "findBookById", query = "SELECT b FROM BOOK b WHERE b.id = :id ")
 public class Book {
 
     @JsonProperty
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     @JsonProperty
     private String name;
--- ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/router/BookRouter.java
+++ ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/router/BookRouter.java
@@ -11,20 +11,34 @@ import com.example.home.ApacheCamelRestExample.pojo.Book;
 @Component
 public class BookRouter extends RouteBuilder {
 
+    // .to("sql:{{sql.selectById}}")
+    // .to("jpa://" + Book.class.getName() + "?query=select b from " + Book.class.getName()
+    // + " b where b.id= :id&parameters={\"id\":${headers.id}}")
+    // .toD("jpa://" + Book.class.getName() + "?namedQuery=findBookById")
+
     @Override
     public void configure() throws Exception {
         restConfiguration().component("servlet").bindingMode(RestBindingMode.json_xml);
 
-        rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route().to("sql:{{sql.selectAll}}")
+        // ok
+        rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route()
+                // .to("sql:{{sql.selectAll}}")
+                .to("jpa://com.example.home.ApacheCamelRestExample.pojo.Book?resultClass=com.example.home.ApacheCamelRestExample.pojo.Book&nativeQuery=select * from library.BOOK")
                 .log("---select all books---");
 
+        // ok 1
         rest().get("book/{id}").description("Details of an book by id").outType(Book.class)
                 .produces(MediaType.APPLICATION_JSON_VALUE).route().log("--- 1 select a book ${body} ---")
-                .to("sql:{{sql.selectById}}").log("--- 2 select a book ${body} ---");
+                .toD("jpa://com.example.home.ApacheCamelRestExample.pojo.Book?query=select b from com.example.home.ApacheCamelRestExample.pojo.Book b where b.id= ${header.id}",
+                        5)
+                .log("--- 2 select a book ${body} ---");
 
-        rest().post("book").produces(MediaType.APPLICATION_JSON_VALUE).route().routeId("postBookRoute")
-                .to("sql:{{sql.insert}}").setHeader(Exchange.HTTP_RESPONSE_CODE, constant(201))
-                .setBody(constant(null));
+        rest().post("book").produces(MediaType.APPLICATION_JSON_VALUE).type(Book.class).route()
+                .routeId("postBookRoute").log("--- binded ---")
+                // .to("sql:{{sql.insert}}")
+                // .toD("jpa://" + Book.class.getName())
+                .to("jpa:com.example.home.ApacheCamelRestExample.pojo.Book&nativeQuery={{sql.insert}}")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(201)).setBody(constant(null));
 
         rest().delete("book/{id}").produces(MediaType.APPLICATION_JSON_VALUE).route().to("sql:{{sql.delete}}")
                 .setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))

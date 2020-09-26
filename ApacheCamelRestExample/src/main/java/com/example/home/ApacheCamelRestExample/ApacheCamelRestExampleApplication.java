5d1bdd0f7336654477847caab93fc9dc052c403b
deesebc <deesebc@gmail.com>
2020-09-26 09:42:28 +0200

    fist approach again

--- ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/ApacheCamelRestExampleApplication.java
+++ ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/ApacheCamelRestExampleApplication.java
@@ -19,7 +19,7 @@ public class ApacheCamelRestExampleApplication {
     public static JaegerTracer getTracer() {
         Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
         Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true);
-        Configuration config = new Configuration("jaeger tutorial").withSampler(samplerConfig).withReporter(reporterConfig);
+        Configuration config = new Configuration("jaeger tutorial 02").withSampler(samplerConfig).withReporter(reporterConfig);
         return config.getTracer();
     }
 

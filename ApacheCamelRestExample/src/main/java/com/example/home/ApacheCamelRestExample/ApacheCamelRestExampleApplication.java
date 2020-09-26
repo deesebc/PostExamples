926c2fcfea93b21fa663162e55329d0e126a5a32
deesebc <deesebc@gmail.com>
2020-09-26 08:39:04 +0200

    add first approach

--- ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/ApacheCamelRestExampleApplication.java
+++ ApacheCamelRestExample/src/main/java/com/example/home/ApacheCamelRestExample/ApacheCamelRestExampleApplication.java
@@ -1,13 +1,26 @@
 package com.example.home.ApacheCamelRestExample;
 
+import org.apache.camel.opentracing.starter.CamelOpenTracing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.JaegerTracer;
 
 @SpringBootApplication
+@CamelOpenTracing
 public class ApacheCamelRestExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApacheCamelRestExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheCamelRestExampleApplication.class, args);
+    }
+    @Bean
+    public static JaegerTracer getTracer() {
+        Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
+        Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true);
+        Configuration config = new Configuration("jaeger tutorial").withSampler(samplerConfig).withReporter(reporterConfig);
+        return config.getTracer();
+    }
 
 }

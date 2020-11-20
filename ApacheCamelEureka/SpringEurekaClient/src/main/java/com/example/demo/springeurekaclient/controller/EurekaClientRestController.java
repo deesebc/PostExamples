b1d240ea717bbd7fe5fa13042a475c9478b7c7f8
deesebc <deesebc@gmail.com>
2020-11-20 13:52:33 +0100

    remove unnecessary library and annotation

--- ApacheCamelEureka/SpringEurekaClient/src/main/java/com/example/demo/springeurekaclient/controller/EurekaClientRestController.java
+++ ApacheCamelEureka/SpringEurekaClient/src/main/java/com/example/demo/springeurekaclient/controller/EurekaClientRestController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EurekaClientRestController {
-
-
     @GetMapping(value="/books", produces = MediaType.APPLICATION_JSON_VALUE)
     public String index() {
         return "[{\"id\":1,\"name\":\"Ender's game\",\"author\":\"Orson Scott Card\"},{\"id\":2,\"name\":\"Foundation\",\"author\":\"Isaac Asimov\"}]";

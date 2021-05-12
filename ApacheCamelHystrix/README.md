13d11af8f5dcd0537a2f9a4efd977a968b298791
Daniel S. Blanco <daniel-santiago.blanco@getronics.com>
2021-05-12 13:33:59 +0200

    add wiremock

---
+++ ApacheCamelHystrix/README.md
@@ -0,0 +1,14 @@
+# Apache Camel Hystrix
+
+Demo of how it works Apache Camel with Hystrix
+
+## Instructions
+
+1. execute command: docker-compose up in src/test/resources/docker folder
+2. execut command: sh run.sh in src/test/resources/wiremock folder
+3. If you open the project with eclipse, run as Java Application the ApacheCamelHystrixExampleApplication.java class
+
+## Explanation
+
+- You can read it in Spanish here:
+- You can read it in English here:
--- ApacheCamelHystrix/src/main/java/com/example/home/ApacheCamelHystrixExample/router/BookHystrixRouter.java
+++ ApacheCamelHystrix/src/main/java/com/example/home/ApacheCamelHystrixExample/router/BookHystrixRouter.java
@@ -13,10 +13,17 @@ public class BookHystrixRouter extends RouteBuilder {
     public void configure() throws Exception {
 	restConfiguration().component("servlet").bindingMode(RestBindingMode.json_xml);
 
-	rest().get("hystrix/book").produces(MediaType.APPLICATION_JSON_VALUE).route().circuitBreaker()
+	rest().get("hystrix/sql").produces(MediaType.APPLICATION_JSON_VALUE).route().circuitBreaker().id("hystrix/sql")
 		.to("sql:{{sql.selectAll}}").onFallback().setBody(bodyAs(ArrayList.class)).endCircuitBreaker()
 		.log("--- SQL select all books---");
 
-    }
+	rest().get("hystrix/http").produces(MediaType.APPLICATION_JSON_VALUE).route().circuitBreaker()
+		.id("hystrix/http").toD("http://localhost:57001/book?bridgeEndpoint=true").onFallback()
+		.setBody(constant("[]")).end().unmarshal().json().log("--- Wiremock select all books---");
 
+	rest().get("hystrix/fail").produces(MediaType.APPLICATION_JSON_VALUE).route().circuitBreaker()
+		.id("hystrix/fail").toD("http://localhost:57001/fail?bridgeEndpoint=true").onFallbackViaNetwork()
+		.toD("http://localhost:57001/book?bridgeEndpoint=true").end().unmarshal().json()
+		.log("--- Wiremock select all books---");
+    }
 }

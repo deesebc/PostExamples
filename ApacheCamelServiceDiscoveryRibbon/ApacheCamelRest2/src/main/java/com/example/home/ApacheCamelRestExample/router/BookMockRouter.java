db06e104e1c47e05983d386d35104f4548441194
Daniel S. Blanco <daniel-santiago.blanco@getronics.com>
2021-06-21 19:06:35 +0200

    SIMPLIFY

--- ApacheCamelServiceDiscoveryRibbon/ApacheCamelRest2/src/main/java/com/example/home/ApacheCamelRestExample/router/BookMockRouter.java
+++ ApacheCamelServiceDiscoveryRibbon/ApacheCamelRest2/src/main/java/com/example/home/ApacheCamelRestExample/router/BookMockRouter.java
@@ -1,43 +1,37 @@
 package com.example.home.ApacheCamelRestExample.router;
 
-import org.apache.camel.Exchange;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import com.example.home.ApacheCamelRestExample.pojo.Book;
-import com.example.home.ApacheCamelRestExample.repository.DBMockRepository;
 
 @Component
 public class BookMockRouter extends RouteBuilder {
+
+    private static Map<Integer, Book> books = new HashMap<>();
+
+    static {
+	books.put(1, new Book(1, "Dune", "Frank Herbert"));
+	books.put(2, new Book(2, "The stars my destination", "Alfred Bester"));
+	books.put(3, new Book(3, "Ender's game", "Orson S. Card"));
+	books.put(4, new Book(4, "Configure Ribbon", "Server 2"));
+    }
 
     @Override
     public void configure() throws Exception {
 	restConfiguration().component("servlet").bindingMode(RestBindingMode.json_xml);
 
-	rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route().bean(DBMockRepository.class, "getAll(})")
+	rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route().bean(BookMockRouter.class, "getAll(})")
 		.marshal().json();
-
-	rest().get("book/{id}").description("Details of an book by id").outType(Book.class)
-		.produces(MediaType.APPLICATION_XML_VALUE).route()
-		.bean(DBMockRepository.class, "findById(${header.id})");
-
-	rest().post("book").type(Book.class).produces(MediaType.APPLICATION_JSON_VALUE).route().routeId("postBookRoute")
-		.bean(DBMockRepository.class, "create(${body})").log("---creating a book---");
-
-	rest().delete("book/{id}").produces(MediaType.APPLICATION_JSON_VALUE).route().bean(DBMockRepository.class,
-		"remove(${header.id})");
-
-	rest().put("book/{id}").type(Book.class).produces(MediaType.APPLICATION_JSON_VALUE).route().choice().when()
-		.simple("${header.id} < 1").bean(BookMockRouter.class, "negativeId").otherwise()
-		.bean(DBMockRepository.class, "update(${header.id}, ${body})");
     }
 
-    public void negativeId(final Exchange exchange) {
-	exchange.getIn().setBody("{\"msg\":\"id can not be negative\"}");
-	exchange.getIn().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-	exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+    public Collection<Book> getAll() {
+	return books.values();
     }
-
 }

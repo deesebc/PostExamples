06326e8a59cc14aa2980ae37ba3ef1a327a8161f
deesebc <deesebc.registros@gmail.om>
2021-08-03 09:29:26 +0200

    ApacheCamelMock connect with consul

--- ApacheCamelConsulExample/ApacheCamelMock/src/main/java/com/example/home/ApacheCamelMock/router/BookMockRouter.java
+++ ApacheCamelConsulExample/ApacheCamelMock/src/main/java/com/example/home/ApacheCamelMock/router/BookMockRouter.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cloud.ServiceDefinition;
+import org.apache.camel.impl.cloud.ServiceRegistrationRoutePolicy;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -23,12 +25,38 @@ public class BookMockRouter extends RouteBuilder {
 	books.put(4, new Book(4, "Configure Ribbon", "Server 2"));
     }
 
+//    @Autowired
+//    private CamelContext context;
+
     @Override
     public void configure() throws Exception {
 	restConfiguration().component("servlet").bindingMode(RestBindingMode.json_xml);
 
-	rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route().bean(BookMockRouter.class, "getAll(})")
-		.marshal().json();
+//	from("servlet://0.0.0.0:9090").routePolicy(new ServiceRegistrationRoutePolicy())
+//		.log("Route ${routeId} has been invoked");
+
+	// OK
+	from("servlet:http://0.0.0.0:9090/api/book").routeId("foo-1")
+		.routeProperty(ServiceDefinition.SERVICE_META_ID, "foo-2")
+		.routeProperty(ServiceDefinition.SERVICE_META_NAME, "mockClient")
+		.routePolicy(new ServiceRegistrationRoutePolicy()).log("Route ${routeId} has been invoked");
+
+	rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route().bean(BookMockRouter.class, "getAll(})");
+
+//	from("direct:registerFooService").setBody()
+//		.constant(
+//			ImmutableRegistration.builder().id("foo-1").name("foo").address("localhost").port(8500).build())
+//		.to("consul:agent?action=REGISTER");
+
+//	from("servlet:http://0.0.0.0:9090").routeId("foo-1")
+//		.routeProperty(ServiceDefinition.SERVICE_META_ID, "foo-1")
+//		.routeProperty(ServiceDefinition.SERVICE_META_NAME, "foo")
+//		.routePolicy(new ServiceRegistrationRoutePolicy());
+
+//	from("servlet:http://0.0.0.0:9090").routePolicy(new ServiceRegistrationRoutePolicy()).routeGroup("my-service")
+//		.routeId("my-id").routeProperty(ServiceDefinition.SERVICE_META_PORT, "8080")
+//		.log("Route ${routeId} has been invoked");
+
     }
 
     public Collection<Book> getAll() {

18783e4e95084d2beebed125155c602246e3187c
deesebc <deesebc.registros@gmail.om>
2021-08-10 16:51:08 +0200

    finish example

--- ApacheCamelConsulExample/ApacheCamelConsul/src/main/java/com/example/home/ApacheCamelConsul/router/BookMockRouter.java
+++ ApacheCamelConsulExample/ApacheCamelConsul/src/main/java/com/example/home/ApacheCamelConsul/router/BookMockRouter.java
@@ -1,12 +1,10 @@
 package com.example.home.ApacheCamelConsul.router;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.cloud.ServiceDefinition;
-import org.apache.camel.component.jackson.JacksonDataFormat;
-import org.apache.camel.impl.cloud.ServiceRegistrationRoutePolicy;
-import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import com.example.home.ApacheCamelConsul.pojo.Book;
@@ -20,23 +18,8 @@ public class BookMockRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-	// dzone way
-
-	restConfiguration().component("servlet").bindingMode(RestBindingMode.json_xml);
-
-	JacksonDataFormat formatBook = new JacksonDataFormat(Book.class);
-
-	from("servlet:http://0.0.0.0:9092/api/book").routeId("ws-foo-1")
-		.routeProperty(ServiceDefinition.SERVICE_META_ID, "ws-foo-2")
-		.routeProperty(ServiceDefinition.SERVICE_META_NAME, "ws-mockClient")
-		.routePolicy(new ServiceRegistrationRoutePolicy()).log("Route ${routeId} has been invoked");
-
-	rest("/book").get("").description("Find account by id").outType(Book.class).route()
-		.serviceCall("consul:mockClient/book").unmarshal(formatBook).endRest();
-
-// ribbon way
-//	rest().get("book").route().log("Route ${routeId} has been invoked 1").removeHeader(Exchange.HTTP_URI)
-//		.serviceCall().name("mockClient/book").log("Route ${routeId} has been invoked 2");
+	rest("/serviceCall").get().produces(MediaType.APPLICATION_JSON_VALUE).outType(Book.class).route()
+		.removeHeader(Exchange.HTTP_URI).serviceCall("mockClient").log("Body: ${body}").endRest();
 
     }
 
--- ApacheCamelConsulExample/ApacheCamelMock/src/main/java/com/example/home/ApacheCamelMock/router/BookMockRouter.java
+++ ApacheCamelConsulExample/ApacheCamelMock/src/main/java/com/example/home/ApacheCamelMock/router/BookMockRouter.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cloud.ServiceDefinition;
 import org.apache.camel.impl.cloud.ServiceRegistrationRoutePolicy;
-import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
@@ -30,18 +29,48 @@ public class BookMockRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-	restConfiguration().component("servlet").bindingMode(RestBindingMode.json_xml);
+//	restConfiguration().component("servlet").bindingMode(RestBindingMode.json_xml);
 
 //	from("servlet://0.0.0.0:9090").routePolicy(new ServiceRegistrationRoutePolicy())
 //		.log("Route ${routeId} has been invoked");
 
+//	from("servlet:http://0.0.0.0:9092").routePolicy(new ServiceRegistrationRoutePolicy()).routeGroup("my-service")
+//		.routeId("my-id").routeProperty(ServiceDefinition.SERVICE_META_PORT, "8080")
+//		.log("Route ${routeId} has been invoked");
+
+//	from("servlet:http://0.0.0.0:9092").routePolicy(new ServiceRegistrationRoutePolicy()).routeGroup("my-service")
+//		.routeId("my-id").routeProperty(ServiceDefinition.SERVICE_META_PORT, "8080")
+//		.log("Route ${routeId} has been invoked");
+
+//	from("direct:start").routePolicy(new ServiceRegistrationRoutePolicy()).routeGroup("client").routeId("mock")
+//		.log("Route ${routeId} has been invoked");
+
+//	from("direct:start").routeId("account-consul").marshal().json(JsonLibrary.Jackson)
+//		.setHeader(Exchange.HTTP_METHOD, constant("PUT"))
+//		.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+//		.setHeader("CamelConsulAction", constant("register"))
+//		.toD("http://localhost:8500/v1/agent/service/register");
+//	from("direct:stop").shutdownRunningTask(ShutdownRunningTask.CompleteAllTasks)
+//		.toD("http://192.168.99.100:8500/v1/agent/service/deregister/${header.id}");
+
 	// OK
-	from("servlet:http://0.0.0.0:9092/api/book").routeId("foo-1")
-		.routeProperty(ServiceDefinition.SERVICE_META_ID, "foo-2")
+//	from("servlet:http://localhost:9092/api/book").routeId("foo-1")
+	from("direct:start").routeId("foo-1").routeProperty(ServiceDefinition.SERVICE_META_ID, "foo-2")
 		.routeProperty(ServiceDefinition.SERVICE_META_NAME, "mockClient")
+		.routeProperty(ServiceDefinition.SERVICE_META_PORT, "9092")
+		.routeProperty(ServiceDefinition.SERVICE_META_HOST, "localhost")
+		.routeProperty("ServiceAddress", "localhost")
+//		.routeProperty(ServiceDefinition.SERVICE_META_PATH, "//localhost:9092/api/book")
+		.routeProperty(ServiceDefinition.SERVICE_META_PATH, "api/book")
 		.routePolicy(new ServiceRegistrationRoutePolicy()).log("Route ${routeId} has been invoked");
 
-	rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route().bean(BookMockRouter.class, "getAll(})");
+	rest().get("book").produces(MediaType.APPLICATION_JSON_VALUE).route().routeId("mockClientGetAll_1")
+		.id("mockClientGetAll").log("mockClient - get - book").bean(BookMockRouter.class, "getAll(})");
+
+	rest().get("book/{id}").produces(MediaType.APPLICATION_JSON_VALUE).route().routeId("mockClientGetBook_1")
+		.id("mockClientGetBook")
+//		.log("mockClient - get - book").bean(BookMockRouter.class, "getById(${exchangeProperty.id})");
+		.log("mockClient - get - book").bean(BookMockRouter.class, "getById(})");
 
 //	from("direct:registerFooService").setBody()
 //		.constant(
@@ -61,5 +90,9 @@ public class BookMockRouter extends RouteBuilder {
 
     public Collection<Book> getAll() {
 	return books.values();
+    }
+
+    public Book getById() {// final Integer id) {
+	return books.get(1);
     }
 }

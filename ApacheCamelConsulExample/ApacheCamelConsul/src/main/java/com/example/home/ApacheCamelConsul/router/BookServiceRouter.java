6611dfe8617910b18983e44017cb3d4d09d9c619
deesebc <deesebc.registros@gmail.om>
2021-08-10 22:09:02 +0200

    reduce code

--- ApacheCamelConsulExample/ApacheCamelConsul/src/main/java/com/example/home/ApacheCamelConsul/router/BookServiceRouter.java
+++ ApacheCamelConsulExample/ApacheCamelConsul/src/main/java/com/example/home/ApacheCamelConsul/router/BookServiceRouter.java
@@ -1,21 +1,14 @@
 package com.example.home.ApacheCamelConsul.router;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookServiceRouter extends RouteBuilder {
-
-    @Autowired
-    CamelContext context;
-
     @Override
     public void configure() throws Exception {
-
 	rest("/serviceCall").get().produces(MediaType.APPLICATION_JSON_VALUE).route().removeHeader(Exchange.HTTP_URI)
 		.serviceCall("bookGetAll").convertBodyTo(String.class).log("Body: ${body}").end();
 
@@ -23,7 +16,5 @@ public class BookServiceRouter extends RouteBuilder {
 		.removeHeader(Exchange.HTTP_PATH).setHeader(Exchange.HTTP_PATH, simple("${header.id}"))
 		.removeHeader(Exchange.HTTP_URI).serviceCall("bookGetById").convertBodyTo(String.class)
 		.log("Body: ${body}").end();
-
     }
-
 }

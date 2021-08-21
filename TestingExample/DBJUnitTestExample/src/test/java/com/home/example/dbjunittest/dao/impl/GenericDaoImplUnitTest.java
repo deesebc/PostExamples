eb1d9aae9d1967df91ee6bdc4084b45776836989
deesebc <deesebc.registros@gmail.om>
2021-08-21 15:26:45 +0200

    improve mock section

--- TestingExample/DBJUnitTestExample/src/test/java/com/home/example/dbjunittest/dao/impl/GenericDaoImplUnitTest.java
+++ TestingExample/DBJUnitTestExample/src/test/java/com/home/example/dbjunittest/dao/impl/GenericDaoImplUnitTest.java
@@ -3,7 +3,7 @@ package com.home.example.dbjunittest.dao.impl;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -14,67 +14,80 @@ import java.util.stream.Stream;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.home.example.dbjunittest.entity.Book;
 
+@ExtendWith(MockitoExtension.class)
 public class GenericDaoImplUnitTest {
-    private static BookDaoImpl dao;
+    BookDaoImpl dao;
 
-    @BeforeAll
-    public static void setUp() {
-	dao = new BookDaoImpl();
-	dao.em = Mockito.mock(EntityManager.class);
-    }
+    @Mock
+    EntityManager eManager;
 
-//	@Mock
-//	TypedQuery mockQuery;
-
-//	@Mock
-//	EntityManager em;
+    @Mock
+    TypedQuery<Book> mockQuery;
 
     @Mock
     Stream<Book> stream;
 
-    @Test
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public void findAll() {
-	TypedQuery mockQuery = mock(TypedQuery.class);
-	Mockito.when(dao.em.createNamedQuery(ArgumentMatchers.anyString(), ArgumentMatchers.eq(Book.class))).thenReturn(mockQuery);
-	Mockito.when(mockQuery.getResultList()).thenReturn(getBooks());
-
-	List<Book> lst = dao.findAll();
-	assertEquals(1, lst.size());
-	Mockito.verify(dao.em).createNamedQuery("Book.findAll", Book.class);
-    }
-
-    private Optional<Book> getBook() {
+    private Book _getBook() {
 	Book book = new Book();
 	book.setId(1);
-	return Optional.of(book);
+	return book;
     }
 
-    private List<Book> getBooks() {
+    private List<Book> _getBooks() {
 	List<Book> lst = new ArrayList<Book>();
 	lst.add(new Book());
 	return lst;
     }
 
+    private Optional<Book> _getOptBook() {
+	Book book = new Book();
+	book.setId(1);
+	return Optional.of(book);
+    }
+
+    @Test
+    public void findAll() {
+	when(dao.em.createNamedQuery(ArgumentMatchers.anyString(), ArgumentMatchers.eq(Book.class))).thenReturn(mockQuery);
+	when(mockQuery.getResultList()).thenReturn(_getBooks());
+
+	List<Book> lst = dao.findAll();
+	assertEquals(1, lst.size());
+	verify(dao.em).createNamedQuery("Book.findAll", Book.class);
+    }
+
     @Test
     public void getByField() {
-	Stream<Book> stream = mock(Stream.class);
-	TypedQuery mockQuery = mock(TypedQuery.class);
 	when(dao.em.createQuery(ArgumentMatchers.anyString(), ArgumentMatchers.eq(Book.class))).thenReturn(mockQuery);
 	when(mockQuery.setParameter("value", "Orson S. Card")).thenReturn(mockQuery);
 	when(mockQuery.getResultStream()).thenReturn(stream);
-	when(stream.findFirst()).thenReturn(getBook());
+	when(stream.findFirst()).thenReturn(_getOptBook());
 
 	Book object = dao.getByField("author", "Orson S. Card");
 	assertThat(object.getId(), equalTo(1));
-	Mockito.verify(dao.em).createQuery("SELECT t FROM Book t where t.author = :value", Book.class);
+	verify(dao.em).createQuery("SELECT t FROM Book t where t.author = :value", Book.class);
+    }
+
+    @Test
+    public void getById() {
+	when(dao.em.find(ArgumentMatchers.eq(Book.class), ArgumentMatchers.eq(1))).thenReturn(_getBook());
+
+	Book object = dao.getById(1);
+	assertThat(object.getId(), equalTo(1));
+	verify(dao.em).find(Book.class, 1);
+    }
+
+    @BeforeEach
+    public void setUp() {
+	dao = new BookDaoImpl();
+	dao.em = eManager;
     }
 }

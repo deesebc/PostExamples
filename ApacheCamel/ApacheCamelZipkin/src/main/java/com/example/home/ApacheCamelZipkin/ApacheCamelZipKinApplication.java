6e92efbeaf30d798f38669fce3302c192a92a549
deesebc <deesebc@gmail.com>
2021-10-11 09:03:26 +0200

    move folders

--- ApacheCamel/ApacheCamelZipkin/src/main/java/com/example/home/ApacheCamelZipkin/ApacheCamelZipKinApplication.java
+++ ApacheCamel/ApacheCamelZipkin/src/main/java/com/example/home/ApacheCamelZipkin/ApacheCamelZipKinApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@CamelZipkin
+k
 public class ApacheCamelZipKinApplication {
 
 	public static void main(final String[] args) {

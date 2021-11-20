32916134f526ce71bf3e6818ca0a353f64f73f4e
deesebc <deesebc.registros@gmail.om>
2021-11-20 09:29:48 +0100

    simplify conf

--- SpringExamples/Spring2Datasources/src/main/java/es/home/example/knowledge/conf/PersistenceProConfiguration.java
+++ SpringExamples/Spring2Datasources/src/main/java/es/home/example/knowledge/conf/PersistenceProConfiguration.java
@@ -1,29 +1,21 @@
 package es.home.example.knowledge.conf;
 
-import java.util.HashMap;
-
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@PropertySource({ "classpath:persistence-multiple-db.properties" })
 @EnableJpaRepositories(basePackages = "es.home.example.knowledge.pro.repository", entityManagerFactoryRef = "proEntityManager", transactionManagerRef = "proTransactionManager")
 public class PersistenceProConfiguration {
-	@Autowired
-	private Environment env;
 
 	@Bean
 	@ConfigurationProperties("app.datasource.pro")
@@ -32,21 +24,9 @@ public class PersistenceProConfiguration {
 	}
 
 	@Bean
-	public LocalContainerEntityManagerFactoryBean proEntityManager(final @Qualifier("proDataSource") DataSource proDataSource) {
-		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-		em.setDataSource(proDataSource);
-		// we can also asociate the entities through schema property in @Entity annotation
-		// Example: @Table(name = "BOOK", schema = "library")
-		em.setPackagesToScan(new String[] { "es.home.example.knowledge.entity" });
-
-		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		em.setJpaVendorAdapter(vendorAdapter);
-		HashMap<String, Object> properties = new HashMap<>();
-		properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-		properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
-		em.setJpaPropertyMap(properties);
-
-		return em;
+	public LocalContainerEntityManagerFactoryBean proEntityManager(final EntityManagerFactoryBuilder builder,
+			final @Qualifier("proDataSource") DataSource proDataSource) {
+		return builder.dataSource(proDataSource).packages("es.home.example.knowledge.entity").build();
 	}
 
 	@Bean

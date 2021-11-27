eb8b52553fd6e18e4ef53fd4b6de2f8a3944e9d3
deesebc <deesebc@gmail.com>
2021-11-27 12:10:55 +0100

    finish example

---
+++ SpringExamples/SpringBootAdminExample/spring-boot-admin-eureka-client/src/main/java/com/baeldung/springbootadminclient/SpringBootAdminEurekaClientApp.java
@@ -0,0 +1,12 @@
+package com.baeldung.springbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminEurekaClientApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminEurekaClientApp.class, args);
+	}
+}

61e80c1c0d99eec2eeedf698d63de8abb60f513d
deesebc <deesebc@gmail.com>
2023-10-28 10:35:13 +0200

    create docker-compose

--- QuarkusExamples/ms-k8s/src/test/java/com/home/example/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.home.example;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
